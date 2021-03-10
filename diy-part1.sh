bbdd3c5dba44bfc778e767649d320101192eeb66
脑袋比地球圆 <59483462+283915025@users.noreply.github.com>
2021-03-10 14:13:18 +0800

    Add files via upload

---
+++ diy-part1.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+#
+# Copyright (c) 2019-2020 P3TERX <https://p3terx.com>
+#
+# This is free software, licensed under the MIT License.
+# See /LICENSE for more information.
+#
+# https://github.com/P3TERX/Actions-OpenWrt
+# File name: diy-part1.sh
+# Description: OpenWrt DIY script part 1 (Before Update feeds)
+#
+
+# 添加SSR-Plus
+# git clone https://github.com/fw876/helloworld.git package/luci-app-ssr-plus
+
+# 添加PassWall
+# git clone https://github.com/xiaorouji/openwrt-passwall.git package/passwall
+
+# 添加bypass
+git clone https://github.com/garypang13/luci-app-bypass.git package/luci-app-bypass
+
+# 添加bypass 依赖
+# svn co https://github.com/garypang13/openwrt-packages/trunk/smartdns
+# svn co https://github.com/garypang13/openwrt-packages/trunk/tcping
+svn co https://github.com/garypang13/openwrt-packages/trunk/lua-maxminddb package/lua-maxminddb
+find package/*/ feeds/*/ -maxdepth 2 -path "*luci-app-bypass/Makefile" | xargs -i sed -i 's/shadowsocksr-libev-ssr-redir/shadowsocksr-libev-alt/g' {}
+find package/*/ feeds/*/ -maxdepth 2 -path "*luci-app-bypass/Makefile" | xargs -i sed -i 's/shadowsocksr-libev-ssr-server/shadowsocksr-libev-server/g' {}
+
+# 添加dnsfilter
+#git clone https://github.com/garypang13/luci-app-dnsfilter.git package/luci-app-dnsfilter
+
+# 添加luci-theme-edge
+git clone -b 18.06 https://github.com/garypang13/luci-theme-edge package/luci-theme-edge
+
+# 添加luci-app-argon-config
+# git clone https://github.com/jerrykuku/luci-app-argon-config package/luci-app-argon-config
+
+# 添加smartdns
+# git clone https://github.com/pymumu/smartdns.git package/smartdns
+# git clone -b lede https://github.com/pymumu/luci-app-smartdns.git package/luci-app-smartdns
---
+++ diy-part2.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+#
+# Copyright (c) 2019-2020 P3TERX <https://p3terx.com>
+#
+# This is free software, licensed under the MIT License.
+# See /LICENSE for more information.
+#
+# https://github.com/P3TERX/Actions-OpenWrt
+# File name: diy-part2.sh
+# Description: OpenWrt DIY script part 2 (After Update feeds)
+#
+
+# 更改默认IP
+sed -i 's/192.168.1.1/192.168.50.1/g' package/base-files/files/bin/config_generate
+
+# 更改管理密码
+#sed -i 's/$1$V4UetPzk$CYXluq4wUazHjmCDBCqXF.:0/$1$LrPFEvqu$gJIceWv7kbIWXHxtFI89Y.:18539/g' package/lean/default-settings/files/zzz-default-settings
+
+# 添加luci-theme-argon
+# rm -rf ../lean/luci-theme-argon
+# git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon package/luci-theme-argon
